28572f9043c5d90597fe8e47c1ed2b10d92096dc
unknown <anands2807@gmail.com>
2023-09-04 21:24:54 +0530

    basic App.js structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,47 @@
+import React, { useState, useEffect } from "react";
+import { useFetch } from "./useFetch";
+import Follower from "./Follower";
 
 function App() {
+  const { loading, data } = useFetch();
+  const [page, setPage] = useState(0);
+  const [followers, setFollowers] = useState([]);
+
   return (
-    <div className="App">
-      <h1>Pagination</h1>
-    </div>
+    <main>
+      <div className="section-title">
+        <h1>{loading ? "loading..." : "pagination"}</h1>
+        <div className="underline"></div>
+      </div>
+      <section className="followers">
+        <div className="container">
+          {followers.map((follower) => {
+            return <Follower key={follower.id} {...follower} />;
+          })}
+        </div>
+        {!loading && (
+          <div className="btn-container">
+            <button className="prev-btn" onClick={() => {}}>
+              prev
+            </button>
+            {data.map((item, index) => {
+              return (
+                <button
+                  key={index}
+                  className={`page-btn ${index === page ? "active-btn" : null}`}
+                  onClick={() => {}}
+                >
+                  {index + 1}
+                </button>
+              );
+            })}
+            <button className="next-btn" onClick={() => {}}>
+              next
+            </button>
+          </div>
+        )}
+      </section>
+    </main>
   );
 }
 
---
+++ src/Follower.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Follower() {
+  return (
+    <div>
+      <h1>Follower</h1>
+    </div>
+  )
+}
+
+export default Follower
